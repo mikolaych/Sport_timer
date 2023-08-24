ffcac124ba56db33db843d8d53c331ec23362022
Коля и Оля <mikolaych@mail.ru>
2023-08-25 00:23:25 +0200

    250823

--- app/src/main/java/com/example/sporttimer/MainActivity.kt
+++ app/src/main/java/com/example/sporttimer/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.sporttimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction().replace(R.id.fragment, Settings()).commit()
